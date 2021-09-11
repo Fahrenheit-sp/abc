cea22894b0c655f25bb4cdd6791de97ef1ec5937
Fahrenheit <igor.maisyk@gmail.com>
2021-09-11 14:01:07 +0300

    Russian language and font

--- ABC/Extensions/UIFont+Foundation.swift
+++ ABC/Extensions/UIFont+Foundation.swift
@@ -10,6 +10,7 @@ import UIKit
 extension UIFont {
 
     static func current(size: CGFloat = 17.0, weight: UIFont.Weight = .regular) -> UIFont {
-        UIFont(name: "PlaytimeWithHotToddies", size: size).or(.systemFont(ofSize: size, weight: weight))
+        let name = Locale.preferredLanguages[0] == "ru" ? "AntipastoPro-Demibold" : "PlaytimeWithHotToddies"
+        return UIFont(name: name, size: size).or(.systemFont(ofSize: size, weight: weight))
     }
 }
